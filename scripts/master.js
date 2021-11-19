a0fd824b9cf4fda790c70a2f140ccbce1bc4883f
oDeltaDois <jaodv1@gmail.com>
2021-11-19 20:28:29 -0300

    js changes

--- scripts/master.js
+++ scripts/master.js
@@ -1,16 +1,18 @@
 let original = document.getElementById('duplicater');
 let i = 0;
+let j = 0;
 // let originalb = document.getElementById('sitename');
 
 
 function duplicate() {
     let clone = original.cloneNode(true); // "deep" clone
-    clone.id = "duplicater" + (++i);
+    clone.id = "duplicater" + (i++);
     // or clone.id = ""; if the divs don't need an ID
     original.parentNode.appendChild(clone);
     let originalkey = document.getElementById('password-value').id;
-    let newkey = originalkey + (++i);
+    let newkey = originalkey + (j++);
     console.log(newkey)
+    document.querySelector('#password-value').id = newkey;
 }
 
 
