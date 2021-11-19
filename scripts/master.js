b1c09b76716806f0abd1314da7f9c8ad0f34e603
oDeltaDois <jaodv1@gmail.com>
2021-11-19 19:48:19 -0300

    passkeeper alpha

---
+++ scripts/master.js
@@ -0,0 +1,22 @@
+let original = document.getElementById('duplicater');
+let i = 0;
+// let originalb = document.getElementById('sitename');
+
+
+function duplicate() {
+    let clone = original.cloneNode(true); // "deep" clone
+    clone.id = "duplicater" + (++i);
+    // or clone.id = ""; if the divs don't need an ID
+    original.parentNode.appendChild(clone);
+    let originalkey = document.getElementById('password-value').id;
+    let newkey = originalkey + (++i);
+    console.log(newkey)
+}
+
+
+// var x = document.getElementById("keyvalue").value;
+// let passwordid = document.getElementById("password-value").id;
+// passwordid = passwordid;
+// console.log(passwordid);
+// document.getElementById("password-value").id = passwordid;
+// // document.getElementById("demo").innerHTML = x;
